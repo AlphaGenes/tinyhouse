5302bdd5f20dbaae660ddfb01a2a619bd576ef73
Steve Thorn <steve.thorn@ed.ac.uk>
2019-08-30 15:24:32 +0100

    Convert multinomial_sample to work with 1D and 2D arrays

--- NumbaUtils.py
+++ NumbaUtils.py
@@ -1,23 +1,29 @@
 from numba import jit, float32
 import numpy as np
 
-@jit(float32[:](float32[:]), nopython=True, nogil=True)
+
+@jit([float32[:](float32[:]), float32[:,:](float32[:,:])], nopython=True, nogil=True)
 def multinomial_sample(pvals):
     """Draw one sample from a multinomial distribution (similar to np.random.multinomial)
-    pvals   - 1D array of probabilities (that should sum to one - but doesn't have to)
-    return  - 1D array of same length as pvals with one value set to 1. and all others 0.
-              this can then be used as an array of probabilities"""
+    pvals   - 1D or 2D array of probabilities (np.float32) (that should sum to one - but doesn't have to)
+    return  - 1D or 2D array (np.float32) of same length as pvals with one value set to 1. and all others 0.
+              this can then be used as an array of probabilities
+    Note: the implementation will work with a pvals array of any dimension, but the function signature
+    limits it to 1D or 1D"""
 
-    # random float in the half-open interval [0.0, np.sum(pvals))
+    # Random float in the half-open interval [0.0, np.sum(pvals))
     rand = np.random.random() * np.sum(pvals)
 
-    # cumulative sum is same type as pvals (float32) so that the sum of pvals is always less than rand
+    # Cumulative sum is same type as pvals so that the total sum of pvals is always less than rand
     cumsum = np.float32(0.0)
-    output_probs = np.zeros_like(pvals, dtype=np.float32)
-    for i, prob in enumerate(pvals):
+
+    # 1D loop over pvals using pvals.ravel()
+    output_probs = np.zeros(pvals.size, dtype=np.float32)
+    for i, prob in enumerate(pvals.ravel()):
         cumsum += prob
         if rand < cumsum:
             output_probs[i] = 1
             break
 
-    return output_probs
+    # Reshape output to be same as pvals shape
+    return output_probs.reshape(pvals.shape)
