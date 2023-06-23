99f75d3c8182714f080fa5b51550924ced62edba
xinger <s2186420@ed.ac.uk>
2023-06-23 17:07:14 +0100

    Add -help argument

--- InputOutput.py
+++ InputOutput.py
@@ -160,12 +160,17 @@ def rawParseArgs(program, parser = None, no_args = False) :
             sys.exit(1)
 
         
-        if (len(args) == 1) and (args[0] not in ["-h", "--help"]):
-            with open(args[0]) as f:
-                args = []
-                for line in f:
-                    if line[0] != "-": line = "-" + line
-                    args.extend(re.split(r"[,|\s|\n]+",line))
+        if len(args) == 1:
+            if args[0] in ["-h", "-help", "--help"]:
+                if args[0] == "-help":
+                    parser.print_help(sys.stderr)
+                    sys.exit(1)
+            else:
+                with open(args[0]) as f:
+                    args = []
+                    for line in f:
+                        if line[0] != "-": line = "-" + line
+                        args.extend(re.split(r"[,|\s|\n]+",line))
         for arg in args:
             if len(arg) == 0:
                 args.remove(arg)
